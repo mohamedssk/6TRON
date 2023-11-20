c2fe60ce6030574ad3fdb5d81693bf173b6bea8a
mohamedssk <104553886+mohamedssk@users.noreply.github.com>
2023-11-20 15:14:04 +0100

    led1

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
-/*
- * Copyright (c) 2022, CATIE
- * SPDX-License-Identifier: Apache-2.0
- */
 #include "mbed.h"
 
 namespace {
 #define PERIOD_MS 2000ms
 }
 
+// Déclarer un objet DigitalIn pour le bouton et un objet DigitalOut pour la LED
+DigitalIn button(BUTTON1);  // USER_BUTTON est généralement défini dans la bibliothèque mbed
+DigitalOut led(LED1);           // LED1 est généralement défini dans la bibliothèque mbed
+
 int main()
 {
-	while (true) {
-		printf("Alive! \n");
-		ThisThread::sleep_for(PERIOD_MS / 2);
-	}
+    while (true) {
+        // Lire l'état du bouton (0 lorsque le bouton est enfoncé, 1 lorsque le bouton est relâché)
+        if (button == 0) {
+            // Allumer la LED lorsque le bouton est enfoncé
+            led = 0;
+        } else {
+            // Éteindre la LED lorsque le bouton est relâché
+            led = 1;
+        }
+
+        ThisThread::sleep_for(PERIOD_MS / 2);
+    }
 }
