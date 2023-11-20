4a4b61f64c2b64a0588baf61c41e9e639510b467
mohamedssk <104553886+mohamedssk@users.noreply.github.com>
2023-11-20 15:49:48 +0100

    interrrupt

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include "mbed.h"
 
 namespace {
-#define PERIOD_MS 2000ms
+#define PERIOD_MS 1000ms
 }
 
-// Déclarer un objet DigitalIn pour le bouton et un objet DigitalOut pour la LED
-DigitalIn button(BUTTON1);  // USER_BUTTON est généralement défini dans la bibliothèque mbed
+InterruptIn button(BUTTON1);  // USER_BUTTON est généralement défini dans la bibliothèque mbed
 DigitalOut led(LED1);           // LED1 est généralement défini dans la bibliothèque mbed
+
+void on_button_rise()
+{
+    led = 1; // Allumer la LED lorsque le bouton est enfoncé
+}
+
+void on_button_fall()
+{
+    led = 0; // Éteindre la LED lorsque le bouton est relâché
+}
+
 
 int main()
 {
-    while (true) {
-        // Lire l'état du bouton (0 lorsque le bouton est enfoncé, 1 lorsque le bouton est relâché)
-        if (button == 0) {
-            // Allumer la LED lorsque le bouton est enfoncé
-            led = 0;
-        } else {
-            // Éteindre la LED lorsque le bouton est relâché
-            led = 1;
-        }
 
-        ThisThread::sleep_for(PERIOD_MS / 2);
+	button.rise(&on_button_rise);  // attacher l'adresse de la fonction on_button_rise à la montée du bouton
+    button.fall(&on_button_fall);  // attacher l'adresse de la fonction on_button_fall à la descente du bouton
+
+    while (true) {
+        ThisThread::sleep_for(PERIOD_MS / 8);
     }
+
 }
